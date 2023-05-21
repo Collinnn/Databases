6c7e1bf579e17e7c6652d67293471a7828f610ad
Collinnn <tobias3060@gmail.com>
2023-05-21 15:14:11 +0200

    Revert "Merge branch 'main' of https://github.com/Collinnn/Databases"
    This reverts commit b277608906c51362acffd5d3b1c89d975a5edc82, reversing
    changes made to 366ac37f6b8ef02fb2138ef5a9ae391ad76de829.

--- Exercises 5.sql
+++ Exercises 5.sql
@@ -14,34 +14,6 @@ DELIMITER ;
 
 select buildingCapacityFct('Watson');
 select * from classroom
-where capacity > buildingCapacityFct('Watson');
+where capacity > buildingCapacityFct('Watson')
 
-DELIMITER //
-create trigger Timeslot_BEFORE_INSERT
-before insert on timeslot for each row
-begin
-	if new.endtime <= new.startTime
-	then signal sqlstate 'HY000'
-		set mysql_errno = 1525,
-        message_text = 'endtime is equal to or after starttime';
-	END IF;
-    if timeoverlapwithTable(new.timeslotID,new.daycode,new.starttime,new.endtime)
-    then signal sqlstate 'HY000' SET mysql_errno = 1525,
-    message_text = 'time interval overlaps with existing timeinterval for the same timeSlotID';
-    end if;
-    END // #END
-    DELIMITER ;
-
-insert timeslot values ('A','T','08:50:00','08:00:00');
-
-SET Global event_scheduler = 1;
-Create Table BallRolls (
-RollNo integer auto_increment primary key,
-LuckyNO integer);
-Create event RollBall
-on schedule every 10 second do
-INSERT BallRolls (luckyNO) values (Floor(37*RAND()));
-
-select * from BallROlls;
-alter event RollBall DISABLE;
 
